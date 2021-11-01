e51c1e83c98fddbd998e2bcf3e68862cbbedddda
Casper Yonel <yonelcasper@gmail.com>
2021-11-01 13:44:44 -0600

    completed

--- server/index.js
+++ server/index.js
@@ -5,8 +5,20 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-let SERVER_PORT = 5050
+let SERVER_PORT = 4000
 
-app.listen(5050, (SERVER_PORT) => {
+
+app.get("/api/users", (req, res) => {
+    let friends = ['Nittin', 'Eric', 'Jeddy', 'Cameron', 'Riley']
+    res.status(200).send(friends)
+})
+
+app.get("/weather/:temperature", (req, res) => {
+    const phrase = `<h3>It was ${req.params.temperature} today</h3>`
+    res.status(200).send(phrase)
+})
+
+app.listen(SERVER_PORT, () => {
     console.log(`the server is running on port ${SERVER_PORT}`)
 })
+
